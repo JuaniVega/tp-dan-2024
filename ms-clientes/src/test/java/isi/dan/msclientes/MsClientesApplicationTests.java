6eab3067b34764467f899053b5cb8e90e81b76b3
Juan Ignacio Vega <63079743+JuaniVega@users.noreply.github.com>
2024-09-14 15:15:06 -0300

    feat: add StateErrorException

--- ms-clientes/src/test/java/isi/dan/msclientes/MsClientesApplicationTests.java
+++ ms-clientes/src/test/java/isi/dan/msclientes/MsClientesApplicationTests.java
@@ -1,13 +1,8 @@
 package isi.dan.msclientes;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsClientesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
