90974efa4e7f5e8abcaf347e625af09ee33254c6
Juan Ignacio Vega <63079743+JuaniVega@users.noreply.github.com>
2024-10-21 03:21:04 -0300

    feat: add new attributes to the Product model

--- ms-productos/src/main/java/isi/dan/ms_productos/modelo/Producto.java
+++ ms-productos/src/main/java/isi/dan/ms_productos/modelo/Producto.java
@@ -1,28 +1,37 @@
 package isi.dan.ms_productos.modelo;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Entity
 @Table(name = "MS_PRD_PRODUCTO")
 @Data
 public class Producto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotNull
-    private String nombre;
-    private String descripcion;
-    @Column(name ="STOCK_ACTUAL")
-    private int stockActual;
-    @Column(name ="STOCK_MINIMO")
-    private int stockMinimo;
-    private BigDecimal precio;
-    
-    @Enumerated(EnumType.STRING)
-    private Categoria categoria;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	private String nombre;
+	private String descripcion;
+	@Column(name = "STOCK_ACTUAL")
+	private int stockActual;
+	@Column(name = "STOCK_MINIMO")
+	private int stockMinimo;
+	private BigDecimal precio;
+	private Float descuento;
+
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
 
 }
