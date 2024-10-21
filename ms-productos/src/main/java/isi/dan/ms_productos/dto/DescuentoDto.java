d41d7a4c1d03bd75acbebe164fbab78750cc0da8
Juan Ignacio Vega <63079743+JuaniVega@users.noreply.github.com>
2024-10-21 03:24:26 -0300

    feat: add new DTO classes to receive as parameter in the ProdController

--- ms-productos/src/main/java/isi/dan/ms_productos/dto/DescuentoDto.java
+++ ms-productos/src/main/java/isi/dan/ms_productos/dto/DescuentoDto.java
@@ -1,14 +1,11 @@
 package isi.dan.ms_productos.dto;
 
-import java.math.BigDecimal;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class StockUpdateDTO {
+public class DescuentoDto {
 	@NotNull(message = "ID must not be null")
 	private Long idProducto;
-	private Integer cantidad;
-	private BigDecimal precio;
+	private Float descuento;
 }
